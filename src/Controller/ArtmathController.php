ed66ba2fc58042730dc396301bfbdbe7c291fce6
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-05-12 17:02:28 +0200

    commit

--- src/Controller/ArtmathController.php
+++ src/Controller/ArtmathController.php
@@ -43,50 +43,49 @@ class ArtmathController extends AbstractController
     /**
      * @Route("/koch", name="app_koch")
      */
-    public function index(): Response
+    public function koch(): Response
     {
-        return $this->render('artmath/index.html.twig', [
+        return $this->render('artmath/koch.html.twig', [
             'fichier' => '',
         ]);
     }
-    
-    public function create_pyprocess():
-    {
-        $args = func_get_args();
-        
-    }
+
     /**
-     * @Route("/calculer", name="calculer")
+     * @Route("/calculer_koch", name="calculer_koch")
      */
-    public function calculer(Request $request): Response
+    public function calculer_koch(Request $request): Response
     {
         // Récupère les paramètres issus du formulaire (on indique le champ name)
         $dimension = $request -> request -> get("dimension") ;
         // Pour les boutons : si appui contenu champ value sinon NULL
         $calculer  = $request -> request -> get("calculer");
-        $imprimer  = $request -> request -> get("imprimer");
-
-
-        // Oui : Appelle le script Python koch.py qui se trouve dans le répertoire /public
-        $process = new Process(['python3','koch.py',$dimension]);
-        $process -> run();
-        // Récupère la valeur de retour renvoyé par le script python
-        $fichier=$process->getOutput();
-
-        // Retourne un message si l'éxécution c'est mal passée
-        if (!$process->isSuccessful())
-            return new Response ("Erreur lors de l'éxécution du script Python :<br>".$process->getErrorOutput());    
-
+        $imprimer  = $request -> request -> get("imprimer");    
+        $out = $this->create_pyprocess("koch.py", $dimension);
         // A t'on appuyé sur calculer ?
         if ($calculer!=NULL)
-            return $this->render('artmath/index.html.twig', [
-                'fichier' => $fichier,
+            return $this->render('artmath/koch.html.twig', [
+                'fichier' => $out,
             ]);
         else {
             // On a appuyé sur imprimer
             return $this->render('artmath/imprimer.html.twig', [
-                'fichier' => $fichier,
+                'fichier' => $out,
             ]);
         }
     }
+
+    public function create_pyprocess(){
+        $args = func_get_args();
+        // Appelle le script Python passer en premier argument qui se trouve dans le répertoire /public
+        $process = new Process(['python3',$args[0],array_slice($args, 1)]);
+        $process -> run();
+        // Récupère la valeur de retour renvoyé par le script python
+        $output=$process->getOutput();
+        if (!$process->isSuccessful())
+            throw new ProcessException("Erreur lors de l'éxécution du script Python :<br>".$process->getErrorOutput());
+        return $output;
+    }
 }
+
+
+class ProcessException extends Exception {}
