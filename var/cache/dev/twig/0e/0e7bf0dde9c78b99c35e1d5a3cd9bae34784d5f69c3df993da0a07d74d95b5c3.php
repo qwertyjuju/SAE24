a07626d33e1c4e2b0d4af573c6541eda9cf4dbc9
qwertyjuju <juju.partage@gmail.com>
2022-05-24 20:48:50 +0100

    commit

--- var/cache/dev/twig/0e/0e7bf0dde9c78b99c35e1d5a3cd9bae34784d5f69c3df993da0a07d74d95b5c3.php
+++ var/cache/dev/twig/0e/0e7bf0dde9c78b99c35e1d5a3cd9bae34784d5f69c3df993da0a07d74d95b5c3.php
@@ -87,8 +87,20 @@ class __TwigTemplate_cdcdaabcbf356bd6673a585f735bd30adfa4a99d4bb8dcc37a623c4516b
         // line 6
         echo "<div>
     <h1>Bienvenue sur le site de l'atelier d'art de Roanne</h1>
-    <p>Sur ce site vous retrouverez differentes oeuvres d'arts générées à partir de plusieurs paramètres.
+    <p>Sur ce site vous retrouverez differentes oeuvres d'arts générées à partir de plusieurs paramètres.<br>
+    Voici quelques exemples d'oeuvres que vous pourrez genérer sur ce site:
     </p>
+    <ul>
+        <li>
+            <h5>suite carré</h5>
+            <img src=\"examples/suite_carre.png\" width=\"50%\">
+        </li>
+        <li>
+            <h5>née carré</h5>
+            <img src=\"examples/nee_carre.png\" width=\"50%\">
+        </li>
+    </ul>
+
 </div>
 ";
         
@@ -123,8 +135,20 @@ class __TwigTemplate_cdcdaabcbf356bd6673a585f735bd30adfa4a99d4bb8dcc37a623c4516b
 {% block body %}
 <div>
     <h1>Bienvenue sur le site de l'atelier d'art de Roanne</h1>
-    <p>Sur ce site vous retrouverez differentes oeuvres d'arts générées à partir de plusieurs paramètres.
+    <p>Sur ce site vous retrouverez differentes oeuvres d'arts générées à partir de plusieurs paramètres.<br>
+    Voici quelques exemples d'oeuvres que vous pourrez genérer sur ce site:
     </p>
+    <ul>
+        <li>
+            <h5>suite carré</h5>
+            <img src=\"examples/suite_carre.png\" width=\"50%\">
+        </li>
+        <li>
+            <h5>née carré</h5>
+            <img src=\"examples/nee_carre.png\" width=\"50%\">
+        </li>
+    </ul>
+
 </div>
 {% endblock %}
 ", "artmath/index.html.twig", "/var/www/artmath/templates/artmath/index.html.twig");
