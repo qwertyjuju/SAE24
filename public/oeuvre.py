be089c9853579fc5a8e61314a30349c58d2fc41d
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-06-08 21:00:51 +0200

    Update oeuvre.py

--- public/oeuvre.py
+++ public/oeuvre.py
@@ -19,7 +19,7 @@ def draw_curve_groups(screen, nb_curves, nb_groups):
     rotate = 0
     while nb_curves > 0:
         a = 1 if not rand_a else rd.uniform(0,1)
-        color = [rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255)]
+        color = [rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255), rd.randint(60, 127)]
         dx, dy = rd.randint(0, 20), rd.randint(0, 50)
         surface = draw_curve_group(int(nb/2), color, a, dx, dy, rotate)
         screen.blit(surface, (0,0))
@@ -28,7 +28,7 @@ def draw_curve_groups(screen, nb_curves, nb_groups):
 
 
 def draw_curve_group(nb, color, a, dx, dy, reverse):
-    surface = pg.Surface((size, size))
+    surface = pg.Surface((size, size), pg.SRCALPHA)
     b = rd.uniform(a, 5.0)
     for i in range(int(nb)):
         draw_curve(surface, a, b, color, dx, dy, 1, reverse)
@@ -41,7 +41,7 @@ def draw_curve(surface, a, b, color, dx, dy, direction=-1, reverse=0):
     xarray = np.arange(size) / size
     yarray = direction * (((xarray ** a) * ((1 - xarray) ** b)) * size) + center
     points = list(zip((xarray * size)+dx, yarray+dy if not reverse else np.flip(yarray)+dy))
-    pg.draw.aalines(surface, color, False, points)
+    pg.draw.lines(surface, color, points)
 
 
 pg.init()
