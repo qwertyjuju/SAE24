e4831126c47c9f1456c0acce2921c945165d579a
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-06-09 14:12:54 +0200

    commit

--- public/oeuvre.py
+++ public/oeuvre.py
@@ -44,7 +44,7 @@ def draw_curve(surface, a, b, color, dx, dy, direction=-1, reverse=0):
     xarray = np.arange(size) / size
     yarray = direction * (((xarray ** a) * ((1 - xarray) ** b)) * size) + center
     points = list(zip((xarray * size)+dx, yarray+dy if not reverse else np.flip(yarray)+dy))
-    pg.draw.lines(surface, color, False, points)
+    pg.draw.aalines(surface, color, False, points)
 
 
 pg.init()
