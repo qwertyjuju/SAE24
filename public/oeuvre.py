6b6f4116785b6283e69fb8d87f43e04fb10b8cc6
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-06-09 09:04:18 +0200

    Update oeuvre.py

--- public/oeuvre.py
+++ public/oeuvre.py
@@ -30,7 +30,7 @@ def draw_curve_groups(screen, nb_curves, nb_groups):
 def draw_curve_group(nb, color, a, dx, dy, reverse):
     surface = pg.Surface((size, size), pg.SRCALPHA)
     b = rd.uniform(a, 5.0)
-    for i in range(int(nb)):
+    for i in range(int(nb)+1):
         draw_curve(surface, a, b, color, dx, dy, 1, reverse)
         draw_curve(surface, a, b, color, dx, dy, -1, reverse)
         b += 0.1
