35583d23914288b628232e0b3a825a31e6a6d0d0
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-06-09 14:24:32 +0200

    Update oeuvre.py

--- public/oeuvre.py
+++ public/oeuvre.py
@@ -41,7 +41,7 @@ def draw_curve_group(nb, color, a, dx, dy, reverse):
 
 
 def draw_curve(surface, a, b, color, dx, dy, direction=-1, reverse=0):
-    xarray = np.arange(dx, size) / size-dx
+    xarray = np.arange(size) / size
     yarray = direction * (((xarray ** a) * ((1 - xarray) ** b)) * size) + center
     points = list(zip((xarray * size)+dx, yarray+dy if not reverse else np.flip(yarray)+dy))
     pg.draw.aalines(surface, color, False, points)
